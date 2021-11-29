fcdf94183a07fe62338d1484481a40094d1ab2fc
Rikkybobby <ricardoreiscorp@gmail.com>
2021-11-29 15:56:21 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("Public"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+//setting up heroku listening port
+let port = process.env.PORT;
+if(port == null || port == "") {
+port = 3000;
+}
+//ejs6 new function syntax
+app.listen(port, () => console.log("server started successfully"));

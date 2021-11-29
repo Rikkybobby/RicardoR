32d6952c7c952d0d05057375b2365c0ca5fa3803
Rikkybobby <ricardoreiscorp@gmail.com>
2021-11-29 18:10:10 +0000

    fixing images

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("Public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
